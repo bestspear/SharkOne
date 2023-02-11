c7b1e3761de7f9dfa90e0dcbcff0279dde5436cf
World peace <68883995+bestspear@users.noreply.github.com>
2023-02-11 08:40:21 +0800

    Delete beacon directory

--- beacon/release64.bat
+++
@@ -1,8 +0,0 @@
-set input=%1%
-@echo off
-mkdir release
-clang++ --target=x86_64-pc-windows-msvc -c c2profile.cpp -o ./release/c2profile.o
-clang++ --target=x86_64-pc-windows-msvc -L ./ beaconMain.cpp ./release/c2profile.o ./release/Utils.o ./release/rotation.o ./release/Global.o ./release/encrypt_decrypt.o ./release/common.o ./release/comm.o ./release/ChildBeacon.o ./release/BeaconX64.o ./release/BeaconTask.o ./release/BeaconSleep.o ./release/Beaconrportfwd.o ./release/BeaconLateralMovement.o ./release/BeaconJob.o ./release/BeaconInject.o ./release/BeaconFileManage.o ./release/BeaconBof.o -o ./release/o.exe
-pe2shc.exe ./release/o.exe ./release/beacon.exe
-echo "success" > %input%
-exit
